8f376763187de216773a17e7155099a09502319f
LucasYang7 <731180001@qq.com>
2016-07-22 12:48:15 +0800

    use selector-syntax to find elements

--- app/src/main/java/com/example/yangzhe/jsoup/JsoupActivity.java
+++ app/src/main/java/com/example/yangzhe/jsoup/JsoupActivity.java
@@ -46,14 +46,17 @@ public class JsoupActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               String[] htmlUrls = {"http://m.58game.com/tu/12204"};
-               new JsoupTask().execute(htmlUrls);
+                String[] htmlUrls = {"http://m.58game.com/tu/12204","http://www.mzitu.com/16993"};
+                new JsoupTask().execute(htmlUrls);
             }
         });
     }
 
+    /**
+     * 使用DOM方法来遍历整个html文档
+     * */
     public String loadDocumentFromURL(String[] htmlUrl){
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder("loadDocumentFromURL:\n");
         try {
             Document document = Jsoup.connect(htmlUrl[0]).get();
             String title = document.title();
@@ -75,6 +78,46 @@ public class JsoupActivity extends AppCompatActivity {
         return result.toString();
     }
 
+    /**
+     * 使用Selector语法来查找元素 注意使用手机访问mzitu.com与使用桌面浏览器访问mzitu.com得到的html文档不同
+     * */
+    public String selectorSyntaxInJsoup(String[] htmlUrl){
+        StringBuilder result = new StringBuilder("selectorSyntaxInJsoup:\n");
+        try {
+            String href = "";
+            String text = "";
+            Document document = Jsoup.connect(htmlUrl[1]).get();
+            result.append(document+"\n\n");
+
+            result.append("所有链接:\n");
+            // 获取当前页面中的所有链接
+            Elements links = document.select("a[href]");
+            for(Element link:links){
+                href = link.attr("href");
+                text = link.text();
+                result.append(href + "\t" + text + "\n");
+            }
+
+            result.append("\n主要图片:\n");
+            // 获取当前页面的显示图片的链接信息
+            Element figureElements = document.select("figure").first();    // 获取figure标签
+            Elements mainImageUrls = figureElements.select("img[src]");
+            for(Element mainImageUrl:mainImageUrls){
+                href = mainImageUrl.attr("src");
+                text = mainImageUrl.attr("alt");
+                result.append(href + "\t" + text + "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * 使用Jsoup解析网页所对应的异步任务
+     * */
     class JsoupTask extends AsyncTask<String,Void,String>{
 
         @Override
@@ -85,7 +128,9 @@ public class JsoupActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... htmlUrl) {
-            String result = loadDocumentFromURL(htmlUrl);
+            String result = "";
+            result += loadDocumentFromURL(htmlUrl) + "\n";
+            result += selectorSyntaxInJsoup(htmlUrl);
             return result;
         }
 
